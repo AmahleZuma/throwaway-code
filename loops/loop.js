c3901dc4327075b618b1d2c2ff38bb105bf21acf
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-03-17 21:23:01 +0200

    Messing around with loops

---
+++ loops/loop.js
@@ -0,0 +1,12 @@
+let number = 0;
+while (number <=100) 
+{
+    console.log(number);
+    number += 1;
+}
+
+for (let count = 0; count <= 2000000000000; count += 1)
+{
+    console.log("You're an idiot")
+    console.log("Dummy")
+}
