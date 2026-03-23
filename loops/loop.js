4314979eb617c3394422b97b7ce46551d7907b0e
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-03-24 00:01:01 +0200

    understanding the guts of the for loop

--- loops/loop.js
+++ loops/loop.js
@@ -1,12 +1,30 @@
-let number = 0;
-while (number <=100) 
-{
-    console.log(number);
-    number += 1;
-}
+// let number = 0;
+// while (number <=100) 
+// {
+//     console.log(number);
+//     number += 1;
+// }
 
-for (let count = 0; count <= 2000000000000; count += 1)
+// for (let count = 0; count <= 200000000000000000; count += 1)
+// {
+//     console.log("You're an idiot")
+//     console.log("Dummy")
+// }
+
+// let counter = {0};
+// let track = 0;
+// while (counter < 10) {
+//     console.log(`Number ${track}`);
+//     counter += 1;
+//     track += 1;
+
+// }
+
+for (let floor = 24; floor <= 30; floor += 1)
 {
-    console.log("You're an idiot")
-    console.log("Dummy")
+    if (floor % 5 == 0)
+    {
+        console.log(floor);
+        break
+    }
 }
