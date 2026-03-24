2108f1f0faeb5398c4fb7864103231fb1d08e98b
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-03-24 15:02:35 +0200

    Triangle problem solved

---
+++ logtriangle/tri.js
@@ -0,0 +1,12 @@
+for (let count = 0;  count <= 6; count += 1) 
+{
+    let block = "#";
+    console.log(block + block.repeat(count))
+}
+
+let triangle = ""
+for (let count = 0; count <= 6; count +=1 )
+{
+    triangle += "#";
+    console.log(triangle)
+}
