3eb01db577a18fad07090caecc7882574846608b
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-03-24 15:03:42 +0200

    comments so I can see

--- logtriangle/tri.js
+++ logtriangle/tri.js
@@ -1,12 +1,13 @@
 for (let count = 0;  count <= 6; count += 1) 
 {
     let block = "#";
-    console.log(block + block.repeat(count))
+    console.log(block + block.repeat(count)) // -> same as below
 }
 
+// empty canvas
 let triangle = ""
 for (let count = 0; count <= 6; count +=1 )
 {
-    triangle += "#";
+    triangle += "#"; // just keeps adding according to to the limit of count - when the program stops
     console.log(triangle)
 }
